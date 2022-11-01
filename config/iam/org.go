643080aee978f3fc942ce78467ac2aa84cf11140
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-11-01 14:14:11 +0100

    Fix comments

--- config/iam/org.go
+++ config/iam/org.go
@@ -2,9 +2,10 @@ package iam
 
 import "github.com/upbound/upjet/pkg/config"
 
-// Configure configures individual resources by adding custom ResourceConfigurators.
+// OrgConfigure configures individual resources by adding custom ResourceConfigurators.
 func OrgConfigure(p *config.Provider) {
 	p.AddResourceConfigurator("hsdp_iam_org", func(r *config.Resource) {
 		r.ShortGroup = shortGroup
+		//r.Kind = "Organization"
 	})
 }
